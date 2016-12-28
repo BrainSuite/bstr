b67f5c0e3c751f1fcd90cdf8cd1a2618ee4785d5
shjoshi <shjoshi@ieee.org>
2016-12-27 20:07:36 -0800

    cbm output methods

--- R/bssoutput_class.R
+++ R/bssoutput_class.R
@@ -40,3 +40,49 @@ setGeneric("save_out", valueClass = "BssOutput", function(bss_out, bss_data, bss
 setMethod("save_out", valueClass = "BssOutput", signature = "BssOutput", function(bss_out, bss_data, bss_model) {
   print("in save")
 })
+
+BssCBMOutput <- setClass(
+  "BssCBMOutput",
+  contains = "BssOutput"
+)
+
+BssTBMOutput <- setClass(
+  "BssTBMOutput",
+  contains = "BssOutput"
+)
+
+BssROIOutput <- setClass(
+  "BssROIOutput",
+  contains = "BssOutput"
+)
+
+setMethod("save_out", valueClass = "BssCBMOutput", signature = "BssCBMOutput", function(bss_out, bss_data, bss_model) {
+
+  log_pvalues <- log10_transform(bss_model@pvalues)
+  s1 <- bss_data@atlas_surface
+
+  s1$attributes <- log_pvalues
+  bss_cmap <- new("BssColormap", "logpvalue", "Spectral", log_pvalues)
+  s1$vColor <- get_colors(bss_cmap)
+  s1$vColor <- matrix(s1$vColor, nrow=3, ncol=s1$hdr$nVertices, byrow = TRUE)
+  writedfs(file.path(bss_out@outdir, 'log_pvalues.dfs'), s1)
+
+  log_pvalues <- log10_transform(p.adjust(bss_model@pvalues, method = 'BH'))
+  s1$attributes <- log_pvalues
+  bss_cmap <- new("BssColormap", "logpvalue", "Spectral", log_pvalues)
+  s1$vColor <- get_colors(bss_cmap)
+  s1$vColor <- matrix(s1$vColor, nrow=3, ncol=s1$hdr$nVertices, byrow = TRUE)
+  writedfs(file.path(bss_out@outdir, 'log_pvalues_adjusted.dfs'), s1)
+
+
+  bss_model@tvalues[abs(log_pvalues) <= -1*log10(0.05)] <- 0
+  bss_cmap <- new("BssColormap", "tvalue", "Spectral", bss_model@tvalues)
+  s1$attributes <- bss_model@tvalues
+  s1$vColor <- get_colors(bss_cmap)
+  s1$vColor <- matrix(s1$vColor, nrow=3, ncol=s1$hdr$nVertices, byrow = TRUE)
+  writedfs(file.path(bss_out@outdir, 'tvalues.dfs'), s1)
+
+  return(bss_out)
+  }
+)
+

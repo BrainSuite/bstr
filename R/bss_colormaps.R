01a61208c54a9b3d6e478133d509b6ac38312ff8
shjoshi <shjoshi@ieee.org>
2016-12-27 22:00:15 -0800

    log pvalue colormap. Also add class method to get different types of colormaps.

--- R/bss_colormaps.R
+++ R/bss_colormaps.R
@@ -24,3 +24,39 @@ setMethod("initialize", valueClass = "BssColormap", signature = "BssColormap",
 setGeneric("get_colors", valueClass = "matrix",function(bss_cmap) {
   standardGeneric("get_colors")
 })
+
+get_logpvalue_colormap <- function(cmap_name, values) {
+  hexcolrs <- RColorBrewer::brewer.pal(11, cmap_name)
+  hexcolrs[6] <- "#FFFFFF"
+  whitecolr <-  "#FFFFFF"
+  colfunc <- colorRampPalette(c("white"))
+  colfunc(10)
+  # First 5 colors are negative
+  # Last 5 colors are positive
+  negcolors <- hexcolrs[1:5]
+  poscolors <- hexcolrs[7:11]
+
+  fnmap <- colorRamp(hexcolrs)
+
+  values_0_to_1 <- ( values - min(values) ) /
+    (max(values) - min(values))
+
+  rgbcolors <- fnmap(values_0_to_1)/255
+  return (rgbcolors)
+
+}
+
+setMethod("get_colors", valueClass = "matrix", signature = "BssColormap", function(bss_cmap) {
+
+  switch(bss_cmap@cmap_type,
+         logpvalue = { bss_cmap@rgbcolors <-
+           get_logpvalue_colormap(bss_cmap@cmap_name, bss_cmap@values)
+         },
+         tvalue = { bss_cmap@rgbcolors <-
+           get_tvalue_colormap(bss_cmap@cmap_name, bss_cmap@values)
+         }
+  )
+
+  return(bss_cmap@rgbcolors)
+
+})

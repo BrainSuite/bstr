dd2d5170dc3a30ff852ce1656aac337010ac71f2
shjoshi <shjoshi@ieee.org>
2017-03-24 20:08:40 -0700

    Check if filename is  character(0).

--- R/bss_utils.R
+++ R/bss_utils.R
@@ -3,12 +3,17 @@
 #' @export
 check_file_exists <- function(filename, raise_error=FALSE, errmesg=NULL) {
   errmesg <- if (is.null(errmesg)) sprintf('File %s does not exist.', filename) else errmesg
-  if ( file.exists(filename) )
-    return(TRUE)
-  else {
-    if (raise_error)
-      stop(errmesg, call. = FALSE)
-    else
+  if (!identical(filename, character(0))) {
+    if ( file.exists(filename) )
+      return(TRUE)
+    else {
+      if (raise_error)
+        stop(errmesg, call. = FALSE)
       return(FALSE)
+    }
+  }
+  else {
+    return(FALSE)
   }
 }
+

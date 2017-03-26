5ed37bbbc49d6c73eee8f754b187acc0e59e01c6
shjoshi <shjoshi@ieee.org>
2017-03-26 16:51:56 -0700

    minor changes to function arguments.

--- R/bss_exec.R
+++ R/bss_exec.R
@@ -1,15 +1,15 @@
 #' Bss executable functions for cbm, tbm, roi etc.
 
 #' @export
-bss_cbm <- function(subjdir, csv, lh_surf_atlas, rh_surf_atlas, sigma_smooth, main_effect, covariates, mspec_file, outdir) {
+bss_cbm <- function(subjdir, csv, lh_surf_atlas, rh_surf_atlas, sigma_smooth, main_effect, covariates, corr_var=NULL, mspec_file, outdir) {
 
   # Left hemisphere
   message('Left hemisphere\n', appendLF = FALSE)
   bss_cbm_data <- new("BssCBMData", subjdir, csv)
   bss_cbm_data <- load_data(bss_cbm_data, atlas_filename = lh_surf_atlas,
                             hemi = 'left', smooth=sigma_smooth)
-  bss_model <- new("BssModel", model_type="bss_lm", main_effect = main_effect, covariates,
-                   demographics = bss_cbm_data@demographics, mspec_file)
+  bss_model <- new("BssModel", model_type="bss_lm", main_effect = main_effect, covariates = covariates,
+                   demographics = bss_cbm_data@demographics, mspec_file=mspec_file)
   bss_model <- run(bss_model, bss_cbm_data)
   bss_output <- new("BssCBMOutput", outdir)
   bss_output <- save_out(bss_output, bss_cbm_data, bss_model)
@@ -19,8 +19,8 @@ bss_cbm <- function(subjdir, csv, lh_surf_atlas, rh_surf_atlas, sigma_smooth, ma
   # bss_cbm_data <- new("BssCBMData", subjdir, csv)
   bss_cbm_data <- load_data(bss_cbm_data, atlas_filename = rh_surf_atlas,
                             hemi = 'right', smooth=sigma_smooth)
-  bss_model <- new("BssModel", model_type="bss_lm", main_effect = main_effect, covariates,
-                   demographics = bss_cbm_data@demographics, mspec_file)
+  bss_model <- new("BssModel", model_type="bss_lm", main_effect = main_effect, covariates = covariates,
+                   demographics = bss_cbm_data@demographics, mspec_file=mspec_file)
   bss_model <- run(bss_model, bss_cbm_data)
   bss_output <- save_out(bss_output, bss_cbm_data, bss_model)
 
@@ -31,8 +31,8 @@ bss_tbm <- function(subjdir, csv, atlas, maskfile, sigma_smooth, main_effect, co
 
   bss_tbm_data <- new("BssTBMData", subjdir, csv)
   bss_tbm_data <- load_data(bss_tbm_data, atlas_filename = atlas, maskfile = maskfile, smooth = sigma_smooth)
-  bss_model <- new("BssModel", model_type="bss_lm", main_effect = main_effect, covariates,
-                   demographics = bss_tbm_data@demographics, mspec_file)
+  bss_model <- new("BssModel", model_type="bss_lm", main_effect = main_effect, covariates=covariates,
+                   demographics = bss_tbm_data@demographics, mspec_file=mspec_file)
   bss_model <- run(bss_model, bss_tbm_data)
   bss_output <- new("BssTBMOutput", outdir)
   bss_output <- save_out(bss_output, bss_tbm_data, bss_model)
@@ -41,8 +41,6 @@ bss_tbm <- function(subjdir, csv, atlas, maskfile, sigma_smooth, main_effect, co
 
 #' @export
 bss_roi <- function(subjdir, csv, roiid, roimeas, main_effect, covariates, mspec_file, outdir) {
-
-  start.time <- Sys.time()
 
   bss_roi_data <- new("BssROIData", subjdir, csv)
   bss_roi_data <- load_data(bss_roi_data, roiid = roiid, roimeas = roimeas, outdir)

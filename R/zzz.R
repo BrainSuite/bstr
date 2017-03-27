053d5b1ec93baac86a9e4e7680ca4c79fe829e99
shjoshi <shjoshi@ieee.org>
2017-03-26 17:04:04 -0700

    invisible return value. Don't want to display messages from onLoad unless absolutely necessary.

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,6 @@
   if(!is_brainsute_installed()) {
     packageStartupMessage(paste('Running setup...', sep = ""), appendLF = FALSE)
     setup(quiet = FALSE, raise_error = FALSE)
+    invisible()
   }
 }

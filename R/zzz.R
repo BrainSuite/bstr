953860ed79e9c042ab8432ce8adfa05cf9412b30
shjoshi <shjoshi@ieee.org>
2017-04-09 21:36:44 -0700

    Added importfrom directives for commonly used methods from standard R packaages.

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,13 @@
+#' @importFrom grDevices col2rgb colorRamp colorRampPalette dev.new
+#' @importFrom graphics axis plot rect
+#' @importFrom methods .valueClassTest new
+#' @importFrom stats formula model.matrix p.adjust pf pt
+#' @importFrom utils read.csv read.table write.csv
+NULL
+
 .onLoad <- function(libname, pkgname) {
 
   if(!is_brainsute_installed()) {
-    packageStartupMessage(paste('Running setup...', sep = ""), appendLF = FALSE)
     setup(quiet = FALSE, raise_error = FALSE)
     invisible()
   }

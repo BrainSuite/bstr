3220b5ee0614ba84646035e8b7657c38586649bb
Yeun Kim <yeunkim@Yeuns-MacBook-Pro.local>
2017-05-02 16:39:36 -0700

    bug fix for linux for finding BS installation path

--- R/bss_setup.R
+++ R/bss_setup.R
@@ -61,18 +61,17 @@ get_brainsuite_path_on_macOS <- function() {
 get_brainsuite_path_on_unix <- function() {
 
   # Search /opt first
-  bs_paths <- sort(list.files('/opt', 'BrainSuite', full.names = TRUE), decreasing = TRUE)[1]
-  if (is.na(bs_paths[1])) {
-    # Search home directory first
-    bs_paths <- sort(list.files(path.expand('~'), 'BrainSuite', full.names = TRUE), decreasing = TRUE)[1]
-    if (!is.na(bs_paths[1])) {
-      # Check if the required files exist
-      if (check_bs_atlas_exists(bs_paths[1], quiet = TRUE, raise_error = FALSE)) return(bs_paths[1]) else return("")
-    }
-    else
-      return(NULL)
-
-  }
+  bs_opt_paths <- sort(list.files('/opt', 'BrainSuite', full.names = TRUE), decreasing = TRUE)[1]
+  bs_home_paths <- sort(list.files(path.expand('~'), 'BrainSuite', full.names = TRUE), decreasing = TRUE)[1]
+  
+  if (check_bs_atlas_exists(bs_opt_paths[1], quiet = TRUE, raise_error = FALSE) && check_bs_atlas_exists(bs_home_paths[1], quiet = TRUE, raise_error = FALSE)) {
+  	bs <- sort(c(basename(bs_opt_paths), basename(bs_home_paths)), decreasing=TRUE)[1]
+  	bs_path <- grep(bs, c(bs_opt_paths, bs_home_paths), value=TRUE)
+  	return(bs_path) 
+  	}
+  if (check_bs_atlas_exists(bs_opt_paths[1], quiet = TRUE, raise_error = FALSE)) return(bs_opt_paths[1]) else return("")
+  if (check_bs_atlas_exists(bs_home_paths[1], quiet = TRUE, raise_error = FALSE)) return(bs_home_paths[1]) else return("")
+  
 }
 
 get_brainsuite_path_on_windows <- function() {

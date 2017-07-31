53f7bc69d8bc6de5a2b30255c8e275e91061e701
Shantanu Joshi <sjoshi@Shantanus-MacBook-Pro.local>
2017-07-30 17:15:02 -0700

    Changed error message. Also fixed bug. Checked for raise_error flag instead of just returning FALSE.

--- R/bss_utils.R
+++ R/bss_utils.R
@@ -22,7 +22,7 @@
 #'
 #' @export
 check_file_exists <- function(filename, raise_error=FALSE, errmesg=NULL) {
-  errmesg <- if (is.null(errmesg)) sprintf('File %s does not exist.', filename) else errmesg
+  errmesg <- if (is.null(errmesg)) sprintf('File or path %s does not exist.', filename) else errmesg
   if (!identical(filename, character(0))) {
     if ( file.exists(filename) )
       return(TRUE)
@@ -33,7 +33,10 @@ check_file_exists <- function(filename, raise_error=FALSE, errmesg=NULL) {
     }
   }
   else {
-    return(FALSE)
+    if (raise_error)
+      stop(errmesg, call. = FALSE)
+    else
+      return(FALSE)
   }
 }
 

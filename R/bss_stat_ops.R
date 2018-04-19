e6f9c6f66b8b6940fd28d830b3e2ebd56257f30a
Kayla Schroeder <kaylaschroeder@Kaylas-MacBook-Pro.local>
2018-04-19 11:43:37 -0700

    column names for roiids changed

--- R/bss_stat_ops.R
+++ R/bss_stat_ops.R
@@ -219,10 +219,9 @@ bss_roi_anova <- function(main_effect="", covariates="", bss_data=bss_data) {
 
   selected_col <- rep(NA, length(bss_data@roiids))
   for (i in 1:length(bss_data@roiids)){
-    selected_col[i] <- paste0(as.character(get_roi_tag(read_label_desc(),bss_data@roiids[i])), " (",bss_data@roiids[i],")")
+    selected_col[i] <- paste0(as.character(get_roi_tag(read_label_desc(),bss_data@roiids[i])), "(",bss_data@roiids[i],")")
     bss_data@demographics[,selected_col[i]]
   }
-  #bss_data@demographics[,as.character(bss_data@roiids)]
 
   cmd1 <- list()
   cmd2 <- list()
@@ -231,10 +230,8 @@ bss_roi_anova <- function(main_effect="", covariates="", bss_data=bss_data) {
 
   for (i in 1:length(bss_data@roiids)){
     cmd1[[i]] <- sprintf("lm_full_%s <- lm(%s, data = bss_data@demographics)",as.character(bss_data@roiids[i]),
-                  #paste('`',as.character(bss_data@roiids[i]),'`', ' ~ ', bss_model@fullmodel, sep = ''))
                   paste('`',as.character(selected_col[i]),'`', ' ~ ', bss_model@fullmodel, sep = ''))
     cmd2[[i]] <- sprintf("lm_null_%s <- lm(%s, data = bss_data@demographics)",as.character(bss_data@roiids[i]),
-                  #paste('`',as.character(bss_data@roiids[i]),'`', ' ~ ', bss_model@fullmodel, sep = ''))
                   paste('`',as.character(selected_col[i]),'`', ' ~ ', bss_model@nullmodel, sep = ''))
     cmd3[[i]] <- sprintf("pander::pander(anova(lm_full_%s, lm_null_%s))",as.character(bss_data@roiids[i]),as.character(bss_data@roiids[i]))
     stats_commands[[i]] <- c(cmd1[[i]], cmd2[[i]], cmd3[[i]])

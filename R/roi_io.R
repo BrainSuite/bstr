4aa7740545ed7a2f0201a347e25ad2978631f084
Kayla Schroeder <kaylaschroeder@Kaylas-MacBook-Pro.local>
2018-05-17 10:28:33 -0700

    added error catch for roiids not in the atlas

--- R/roi_io.R
+++ R/roi_io.R
@@ -65,6 +65,9 @@ read_roi_data_for_all_subjects <- function(roi_filelist, demographics,roiids, ro
   roi_data_frame <- data.frame(matrix(nrow=length(roi_filelist),ncol=length(roiids)))
   rownames(roi_data_frame) <- demographics$subjID
   for (i in 1:length(roiids)){
+    if (!(roiids[i] %in% read_label_desc()$roiid)){
+      stop(sprintf('ROI ID %d not found. Please check that %d is a valid ROI.', roiids[i], roiids[i]))
+    }
     colnames(roi_data_frame)[i] <- paste0(as.character(get_roi_tag(read_label_desc(),roiids[i])), "(",roiids[i],")")
   }
 

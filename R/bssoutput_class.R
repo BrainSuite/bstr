9e1bf27eba28d6b60541fe5aaeaa0c2f95e7138e
kaylaschroeder <kaylaschroeder@g.ucla.edu>
2019-02-20 12:33:28 -0800

    minor lm_vec and voxelcoord call changes

--- R/bssoutput_class.R
+++ R/bssoutput_class.R
@@ -637,7 +637,7 @@ save_vol_stats_out <- function(log_pvalues, log_pvalues_adjusted, tvalues, tvalu
 
 get_voxelcoord <- function(bss_out, bss_data, bss_model, outdir, nclusters){
   # Call cluster code from terminal
-  voxelcoord_call <- paste0("clustermap -i ", outdir,bss_model@model_type,"_",bss_model@main_effect,
+  voxelcoord_call <- paste0("clustermap -i ", outdir,"/",bss_model@model_type,"_",bss_model@main_effect,
                             "_", tools::file_path_sans_ext(basename(bss_data@atlas_filename)), "_tvalues_adjusted.nii.gz",
                             " -m ", bss_data@maskfile, " -o ", outdir, "/cluster.tsv", " -n ", nclusters)
 
@@ -651,7 +651,7 @@ get_voxelcoord <- function(bss_out, bss_data, bss_model, outdir, nclusters){
   }
   # Use tvalues instead of adjusted tvalues if no clusters are found
   if (length(voxelcoord) == 0){
-    voxelcoord_call <- paste0("clustermap -i ", outdir,bss_model@model_type,"_",bss_model@main_effect,
+    voxelcoord_call <- paste0("clustermap -i ", outdir,"/",bss_model@model_type,"_",bss_model@main_effect,
                               "_", tools::file_path_sans_ext(basename(bss_data@atlas_filename)), "_tvalues.nii.gz",
                               " -m ", bss_data@maskfile, " -o ", outdir, "/cluster.tsv", " -n ", nclusters)
 

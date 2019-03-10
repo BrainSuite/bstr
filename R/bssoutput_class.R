789a9589b4ee4b05ad3641d3be92382689f0da1e
shjoshi <shjoshi@ieee.org>
2019-03-09 16:10:42 -0800

    document arguments.

--- R/bssoutput_class.R
+++ R/bssoutput_class.R
@@ -624,7 +624,7 @@ save_bss_out <- function(bss_data, bss_model, outdir="", overwrite = F, ncluster
 #' Save the surface output to the given ouput directory
 #' @param measure numeric value denoting the measures used to create the output
 #' @param var_name string denoting name of variable used by the function
-#' @param cmap_title string denoting the type of color map
+#' @param bss_cmap object of type \code{BssColormap}
 #' @param bss_data object of type \code{BssData}
 #' @param bss_model object of type \code{BssModel}
 #' @param outdir string specifying output directory to save the results in
@@ -644,7 +644,7 @@ save_bss_out_surface <- function(measure, var_name, bss_cmap, bss_data, bss_mode
 #' Save the nifti image to the output file
 #' @param measure denotes the measure used to create the output
 #' @param var_name string denoting name of variable used by the function
-#' @param cmap_title string denoting the type of color map
+#' @param bss_cmap object of type \code{BssColormap}
 #' @param bss_data object of type \code{BssData}
 #' @param bss_model object of type \code{BssModel}
 #' @param outdir string specifying output directory to save the results in

e560094916617959e738c63aece46271d911f314
Shantanu Joshi <shj2545@gmail.com>
2023-11-29 12:01:31 -0800

    bugfix: Show correlations in the html cluster table for correlation analysis and t statistics for other types of analyses.

--- R/bssoutput_class.R
+++ R/bssoutput_class.R
@@ -852,9 +852,21 @@ get_voxelcoord <- function(bstr_out, bstr_data, bstr_model, outdir, nclusters){
          pairedttest = {var_name = bstr_model@group_var},
          unpairedttest = {var_name = bstr_model@group_var}
   )
+
+  if (bstr_model@model_type == "bstr_corr") {
+    file_suffix_adjusted <- bs_stat_overlays$corr_values_masked_adjusted
+    file_suffix <- bs_stat_overlays$corr_values
+  } else {
+    file_suffix_adjusted <- bs_stat_overlays$tvalues_adjusted
+    file_suffix <- bs_stat_overlays$tvalues
+  }
+
   voxelcoord_call <- paste0("\"",file.path(get_brainsuite_install_path(),bs_binary_files$clustermap),"\" -i \"", outdir,"/",bstr_model@model_type,
-                            "_",var_name,"_", tools::file_path_sans_ext(basename(bstr_data@atlas_filename)),
-                            "_tvalues_adjusted.nii.gz\""," -m \"", bstr_data@maskfile, "\" -o \"", outdir, "/cluster.tsv\"", " -n ", nclusters)
+                            "_",var_name,"_", tools::file_path_sans_ext(basename(bstr_data@atlas_filename)), "_", file_suffix_adjusted, ".nii.gz\""," -m \"", bstr_data@maskfile, "\" -o \"", outdir, "/cluster.tsv\"", " -n ", nclusters)
+
+  # voxelcoord_call <- paste0("\"",file.path(get_brainsuite_install_path(),bs_binary_files$clustermap),"\" -i \"", outdir,"/",bstr_model@model_type,
+  #                           "_",var_name,"_", tools::file_path_sans_ext(basename(bstr_data@atlas_filename)),
+  #                           "_tvalues_adjusted.nii.gz\""," -m \"", bstr_data@maskfile, "\" -o \"", outdir, "/cluster.tsv\"", " -n ", nclusters)
   system(voxelcoord_call,intern=FALSE, ignore.stdout=FALSE, ignore.stderr=FALSE, wait=TRUE, input=NULL)
   if(file.info(paste0(outdir, "/cluster.tsv"))$size!=0){
     vox_table <- read.table(paste0(outdir,"/cluster.tsv"),header=F,sep="\t")
@@ -868,8 +880,11 @@ get_voxelcoord <- function(bstr_out, bstr_data, bstr_model, outdir, nclusters){
   # Use tvalues instead of adjusted tvalues if no clusters are found
   if (file.info(paste0(outdir, "/cluster.tsv"))$size==0){
     voxelcoord_call <- paste0("\"",file.path(get_brainsuite_install_path(),bs_binary_files$clustermap),"\" -i \"", outdir,"/",bstr_model@model_type,
-                              "_",var_name,"_", tools::file_path_sans_ext(basename(bstr_data@atlas_filename)), "_tvalues.nii.gz\"",
-                              " -m \"", bstr_data@maskfile, "\" -o \"", outdir, "/cluster.tsv\"", " -n ", nclusters)
+                              "_",var_name,"_", tools::file_path_sans_ext(basename(bstr_data@atlas_filename)), "_", file_suffix_adjusted, ".nii.gz\""," -m \"", bstr_data@maskfile, "\" -o \"", outdir, "/cluster.tsv\"", " -n ", nclusters)
+
+#    voxelcoord_call <- paste0("\"",file.path(get_brainsuite_install_path(),bs_binary_files$clustermap),"\" -i \"", outdir,"/",bstr_model@model_type,
+#                              "_",var_name,"_", tools::file_path_sans_ext(basename(bstr_data@atlas_filename)), "_tvalues.nii.gz\"",
+#                              " -m \"", bstr_data@maskfile, "\" -o \"", outdir, "/cluster.tsv\"", " -n ", nclusters)
     system(voxelcoord_call,intern=FALSE, ignore.stdout=FALSE, ignore.stderr=FALSE, wait=TRUE, input=NULL)
     if(file.info(paste0(outdir, "/cluster.tsv"))$size!=0){
       vox_table <- read.table(paste0(outdir,"/cluster.tsv"),header=F,sep="\t")

ca7f1aadb2362c26b32ffd638891531d3c6d6806
dshattuck <david.shattuck@gmail.com>
2024-09-24 22:36:03 -0700

    changed copyrights to 2024

--- R/bstr_utils.R
+++ R/bstr_utils.R
@@ -1,5 +1,5 @@
 # BrainSuite Statistics Toolbox in R (bstr)
-# Copyright (C) 2017 The Regents of the University of California
+# Copyright (C) 2024 The Regents of the University of California
 # Creator: Shantanu H. Joshi, Department of Neurology, Ahmanson Lovelace Brain Mapping Center, UCLA
 #
 # This program is free software; you can redistribute it and/or modify it under the terms
